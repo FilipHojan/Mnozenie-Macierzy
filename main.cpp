55e9e7b365eb8055bd801f8e5244d7b831249b2e
FilipHojan <81771162+FilipHojan@users.noreply.github.com>
2025-02-03 21:55:21 +0100

    small fix

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@
 #include "omp.h"
 #include <cmath>
 
-static const int ROWS = 1000;     // liczba wierszy macierzy
-static const int COLUMNS = 1000;  // lizba kolumn macierzy
+static const int ROWS = 1000;     // liczba wierszy macierzy  2048/3072/4096
+static const int COLUMNS = 1000;  // lizba kolumn macierzy	  2048/3072/4096
 float matrix_a[ROWS][COLUMNS];    // lewy operand 
 float matrix_b[ROWS][COLUMNS];    // prawy operand
 float matrix_r[ROWS][COLUMNS];    // wynik
@@ -21,58 +21,15 @@ float matrix_r1[ROWS][COLUMNS];
 FILE *result_file;
 
 void initialize_matrices();
+
 void clear_result_matrix();
+
 void print_elapsed_time(double start, char* description);
-/*
-	Sprobujmy opisac co moze byc przyczyna takiego a nie innego czasu przetwarania
-*/
+
 void multiply_matrices_IJK();
-/*
-	To samo co wyzej tylko w 6 petlach
-*/
+
 void multiply_matrices_IJK_IKJ();
 
-
-int main(int argc, char* argv[]) {
-	if ((result_file = fopen("classic.txt", "a")) == NULL) {
-		fprintf(stderr, "nie mozna otworzyc pliku wyniku \n");
-		perror("classic");
-		return(EXIT_FAILURE);
-	}
-
-	double start = clock() / CLK_TCK;
-	initialize_matrices();
-	print_elapsed_time(start, "Inicjalizacja macierzy");
-
-	clear_result_matrix();
-	start = clock() / CLK_TCK;
-	multiply_matrices_IJK();
-	print_elapsed_time(start, "Mno�enie macierzy 3 p�tle IJK");
-
-	//ONLY TO TEST IF 6LOOP method is correct
-	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j < COLUMNS; j++) {
-			matrix_r1[i][j] = matrix_r[i][j];
-		}
-	}
-
-	clear_result_matrix();
-	start = clock() / CLK_TCK;
-	multiply_matrices_IJK_IKJ();
-	print_elapsed_time(start, "Mnozenie macierzy 6 petli IJK-IKJ");
-
-	//ONLY TO TEST IF 6LOOP method is correct
-	for (int i = 0; i < ROWS; i++) {
-		for (int j = 0; j < COLUMNS; j++) {
-			if (abs(matrix_r1[i][j] - matrix_r[i][j]) > 0.001  ) {
-				printf("%f != %f\n", matrix_r1[i][j], matrix_r[i][j]);
-			}
-		}
-	}
-	
-	fclose(result_file);
-	return 0;
-}
 
 
 void multiply_matrices_IJK()
@@ -81,8 +38,9 @@ void multiply_matrices_IJK()
 	for (int i = 0; i < ROWS; i++) {
 		for (int j = 0 ; j < COLUMNS; j++) {
 			float sum = 0.0;
-			for (int k = 0 ; k < COLUMNS; k++)
+			for (int k = 0 ; k < COLUMNS; k++){
 				sum += matrix_a[i][k] * matrix_b[k][j];
+			}
 			matrix_r[i][j] = sum;
 		}
 	}
@@ -123,9 +81,11 @@ void initialize_matrices()
 void clear_result_matrix()
 {
 	#pragma omp parallel for 
-	for (int i = 0; i < ROWS; i++)
-		for (int j = 0; j < COLUMNS; j++)
+	for (int i = 0; i < ROWS; i++){
+		for (int j = 0; j < COLUMNS; j++){
 			matrix_r[i][j] = 0.0;
+		}
+	}
 }
 
 void print_elapsed_time(double start, char* description)
@@ -135,7 +95,48 @@ void print_elapsed_time(double start, char* description)
 	// wyznaczenie i zapisanie czasu przetwarzania
 	elapsed = (double)clock() / CLK_TCK;
 	printf("%s \t\t| czas: %8.4f sec \n", description, elapsed - start);
-	fprintf(result_file,
-		"%s \t\t| czas : %8.4f sec (%6.4f sec rozdzielczosc pomiaru)\n",
-		description, elapsed-start, 1.0 / CLK_TCK);
+	fprintf(result_file, "%s \t\t| czas : %8.4f sec (%6.4f sec rozdzielczosc pomiaru)\n", description, elapsed-start, 1.0 / CLK_TCK);
+	
+}
+
+
+int main(int argc, char* argv[]) {
+	if ((result_file = fopen("classic.txt", "a")) == NULL) {
+		fprintf(stderr, "nie mozna otworzyc pliku wyniku \n");
+		perror("classic");
+		return(EXIT_FAILURE);
+	}
+
+	double start = clock() / CLK_TCK;
+	initialize_matrices();
+	print_elapsed_time(start, "Inicjalizacja macierzy");
+
+	clear_result_matrix();
+	start = clock() / CLK_TCK;
+	multiply_matrices_IJK();
+	print_elapsed_time(start, "Mnozenie macierzy 3 petle IJK");
+
+	//ONLY TO TEST IF 6LOOP method is correct
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			matrix_r1[i][j] = matrix_r[i][j];
+		}
+	}
+
+	clear_result_matrix();
+	start = clock() / CLK_TCK;
+	multiply_matrices_IJK_IKJ();
+	print_elapsed_time(start, "Mnozenie macierzy 6 petli IJK-IKJ");
+
+	//ONLY TO TEST IF 6LOOP method is correct
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLUMNS; j++) {
+			if (abs(matrix_r1[i][j] - matrix_r[i][j]) > 0.001  ) {
+				printf("%f != %f\n", matrix_r1[i][j], matrix_r[i][j]);
+			}
+		}
+	}
+	
+	fclose(result_file);
+	return 0;
 }
